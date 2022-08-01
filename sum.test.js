b9eb62c9dcb9a940693f1734317a3a049f3e5555
Kondaspoorthy <kondasaispoorthy@msitprogram.net>
2022-08-01 12:35:43 +0530

    updated2

--- sum.test.js
+++ sum.test.js
@@ -3,6 +3,6 @@ const sub1 = require('./functions')
 test('adds 1 + 2 to equal 3', () => {
   expect(sum1.sum(1, 2)).toBe(3);
 });
-test('adds 1 + 2 to equal 3', () => {
-    expect(sub1.sub(2, 1)).toBe(1);
+test('subs 2-1 equal to 3', () => {
+    expect(sub1.sub(2, 1)).toBe(3);
   });
